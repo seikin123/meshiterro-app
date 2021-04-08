c5a19906be5a259ed277ce6a14debfa6af857038
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-08 13:35:09 +0900

    [修正]top画面へ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'post_images#index'
+  root to: 'homes#top'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
