3e95209e3b6c7fc410cf28505529f2dedefa87d7
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-08 12:18:39 +0900

    [add]1:Nの関係性をモデルに実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_024146) do
+ActiveRecord::Schema.define(version: 2021_04_08_031451) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
