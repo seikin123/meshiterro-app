88cf0606c23c7e2f96ab8424a7a20bf793ffb681
ヨシダタイキ <abc521fjr7@gmail.com>
2021-04-08 13:53:14 +0900

    [add]post_comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_031451) do
+ActiveRecord::Schema.define(version: 2021_04_08_044713) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
